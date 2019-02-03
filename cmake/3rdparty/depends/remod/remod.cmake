1433147a424dd712a563a07eadce39227e6fbc44
Kevsoft <kevin.waldock@gmail.com>
2019-02-03 12:09:14 +0100

    Attempt to fix linker error

--- cmake/3rdparty/depends/remod/remod.cmake
+++ cmake/3rdparty/depends/remod/remod.cmake
@@ -4,20 +4,22 @@ find_package(Git REQUIRED)
 set(REMOD_INSTALL_DIR ${CMAKE_BINARY_DIR}/external/remod-install)
 set(REMOD_INCLUDE_DIR ${REMOD_INSTALL_DIR}/include)
 set(REMOD_LIBRARY_DIR ${REMOD_INSTALL_DIR}/lib)
-set(REMOD_LIBRARY_FILES ${REMOD_LIBRARY_DIR}/ReModCore.lib)
+set(REMOD_LIBRARY_FILES ${REMOD_LIBRARY_DIR}/ReModCore.lib ${REMOD_LIBRARY_DIR}/asmjit.lib)
 
 ExternalProject_Add(
     remod
     PREFIX ${CMAKE_BINARY_DIR}/external/remod
     GIT_REPOSITORY https://github.com/KevinW1998/ReMod-Core.git
     # GIT_TAG v2.0.1 # No git tag for now...
-    GIT_TAG 9705d49fe539426be0a577f6c886209bc2f04d9a
+    GIT_TAG 30fe0d401bef0f5cfd49ae5c22b87fc204e139e5
     TIMEOUT 10
     # UPDATE_COMMAND "git pull"
     LOG_DOWNLOAD ON
     BUILD_IN_SOURCE 1
     CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${REMOD_INSTALL_DIR}" "-DBUILD_SHARED_LIBS=OFF"
 )
+
+add_definitions(-DASMJIT_STATIC)
 
 function(remod_add_dependency target)
     target_include_directories(${target} PUBLIC ${REMOD_INCLUDE_DIR})

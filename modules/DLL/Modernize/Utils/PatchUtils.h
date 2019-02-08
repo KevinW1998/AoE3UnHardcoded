d464d0b46186ed60b844ffe7ab9aa568c7b9ad7b
Kevsoft <kevin.waldock@gmail.com>
2019-02-08 19:54:37 +0100

    Fixed crash

--- modules/DLL/Modernize/Utils/PatchUtils.h
+++ modules/DLL/Modernize/Utils/PatchUtils.h
@@ -5,9 +5,7 @@ namespace UHCDLL::PatchUtils
 {
 	template<typename ResolveStrategy>
 	remod::trackable_wrapper<remod::trackable_memory_patch> AddPatchAndApply(remod::patch_manager<ResolveStrategy>& manager, AsmPatch::AsmPatchData data) {
-		
-		std::pair<std::uintptr_t, std::vector<std::uint8_t>> dataPack = std::move(data.getDataPack());
-		remod::trackable_wrapper<remod::trackable_memory_patch> patch = manager.create_trackable_memory_patch_with_data(dataPack.first, dataPack.second);
+		remod::trackable_wrapper<remod::trackable_memory_patch> patch = manager.create_trackable_memory_patch_with_data(data.getAddress(), std::move(data.getDataRef()));
 		patch->patch();
 
 		return patch;
